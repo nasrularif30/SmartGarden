42c232cb5a560a325d2daa2a89df78021f8b5249
nasrularif30 <46752379+nasrularif30@users.noreply.github.com>
2021-05-07 20:38:01 +0700

    Initial commit

---
+++ app/src/main/java/com/movtech/smartgarden/BerandaUser.java
@@ -0,0 +1,14 @@
+package com.movtech.smartgarden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BerandaUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda_user);
+    }
+}
---
+++ app/src/main/java/com/movtech/smartgarden/LoginRole.java
@@ -0,0 +1,14 @@
+package com.movtech.smartgarden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginRole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_role);
+    }
+}
